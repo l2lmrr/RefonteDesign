50711174e3627dd5db2bb4b80a3c8e49378d8d27
younes <younes.imouga@gmail.com>
2024-10-22 11:39:38 +0100

    ServicePart

---
+++ css/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      "./src/*/.{html,js}",
+    ],
+    theme: {
+    screens: {
+        sm: '420px',
+        md: '768',
+        lg:'976',
+        xl:'1440px'
+    },
+      extend: {
+      },
+    },
+    plugins: [],
+  };
