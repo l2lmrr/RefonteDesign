f59d9abee16e898864af1f9ed439e85373801c15
unknown <issammahtaj02@gmail.com>
2024-10-22 20:55:21 +0100

    blog part

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [
+      "./src/*/.{html,js}",
+    ],
+    theme: {
+      extend: {
+      },
+    },
+    plugins: [],
+  };
+
+  module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'xs': '390px', 
+        },
+      },
+    },
+  };
+  
